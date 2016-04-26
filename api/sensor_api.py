7b2da34c24146cb2a15c61d9aea37b9232e582e0
Jacob Sohl <jacobsohl@gmail.com>
2016-04-25 20:25:09 -0700

    ignore stuff

--- api/sensor_api.py
+++ api/sensor_api.py
@@ -2,12 +2,24 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    msg = "ESP Sensor!\n\
-          Firmware: 0.1.2\n\
-          (c) Jacob Sohl\n\
-          MIT License"
+def root():
+    msg = "ESP Sensor"
+    return msg
+
+@app.route("/api/v1/temp")
+def temp():
+    msg = "71"
+    return msg
+
+@app.route("/api/v1/humid")
+def humid():
+    msg ="humidity: 83%"
+    return msg
+
+@app.route("/api/v1/firmware")
+def firmware():
+    msg ="Firmware: 0.1.2"
     return msg
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
